24a4fe66fd243450ff1344db73d5d922a570de14
Olga Vorobeva <ms.olya_v>
2023-02-01 17:51:49 +0300

    fix

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,17 +4,22 @@ from brain_games.games.greeting import greeting
 from brain_games.games.game_logic import logic
 
 
+def check_prime(a):
+    prime = True
+    j = 2
+    while j <= sqrt(a) and prime is True:
+        if a % j == 0:
+            prime = False
+        j += 1
+    return prime
+
+
 def one_round():
     digit = randint(1, 100)
     print(f'Question: {digit}')
     p = input('Your answer:')
-    prime = True
-    j = 2
-    while j <= sqrt(digit) and prime is True:
-        if digit % j == 0:
-            prime = False
-        j += 1
-    is_prime = 'yes' if prime else 'no'
+    is_prime = check_prime(digit)
+    is_prime = 'yes' if is_prime else 'no'
     if p == is_prime:
         return True, is_prime, p
     else:
